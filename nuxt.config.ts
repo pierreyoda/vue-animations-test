bea689ef93f99ceca5bed2f085457e7e4fb129a0
pierreyoda <pierreyoda@users.noreply.github.com>
2019-07-26 00:04:25 +0200

    :arrow_up: Upgrade dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -63,36 +63,38 @@ const config: NuxtConfiguration = {
       plugins: [
         require("tailwindcss"),
         require("autoprefixer"),
-        production && require("@fullhuman/postcss-purgecss")({
-          content: [
-            "*.html",
-            "components/**/*.vue",
-            "layouts/**/*.vue",
-            "pages/**/*.vue",
-            "plugins/**/*.ts",
-          ],
-          whitelist: ["html", "body", "nuxt-progress", "nuxt-link-active", "nuxt-link-exact-active"],
-          whitelistPatterns: [
-            /-enter-active$/,
-            /-leave-active$/,
-            /-enter$/,
-            /-leave-to$/,
-          ],
-          styleExtensions: [".scss", ".css"],
-          extractors: [
-            {
-              extractor: class TailwindExtractor  {
-                static extract(content: string) {
-                  return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-                }
+        ...production ? [
+          require("@fullhuman/postcss-purgecss")({
+            content: [
+              "*.html",
+              "components/**/*.vue",
+              "layouts/**/*.vue",
+              "pages/**/*.vue",
+              "plugins/**/*.ts",
+            ],
+            whitelist: ["html", "body", "nuxt-progress", "nuxt-link-active", "nuxt-link-exact-active"],
+            whitelistPatterns: [
+              /-enter-active$/,
+              /-leave-active$/,
+              /-enter$/,
+              /-leave-to$/,
+            ],
+            styleExtensions: [".scss", ".css"],
+            extractors: [
+              {
+                extractor: class TailwindExtractor {
+                  static extract(content: string) {
+                    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+                  }
+                },
+                extensions: ["html", "vue", "js", "ts"],
               },
-              extensions: ["html", "vue", "js", "ts"],
-            },
-          ],
-        }),
-        production && require("cssnano")({
-          preset: "default",
-        }),
+            ],
+          }),
+          require("cssnano")({
+            preset: "default",
+          }),
+        ] : [],
       ],
     },
     // https://championswimmer.in/vuex-module-decorators/pages/installation.html#es5-transpilation
